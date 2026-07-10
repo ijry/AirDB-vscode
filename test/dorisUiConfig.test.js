c07e7f0d5752fcc2878efec6ffd7fb4f8e9347ce
jry <ijry@qq.com>
2026-07-11 05:27:29 +0800

    fix: stabilize redshift ui regressions

--- test/dorisUiConfig.test.js
+++ test/dorisUiConfig.test.js
@@ -10,8 +10,8 @@ const connect = read("src/vue/connect/index.vue");
 
 assert.match(connect, /Doris:\s*\{[\s\S]*icon: require\("@\/\.\.\/resources\/icon\/doris\.svg"\)/);
 assert.match(connect, /supportDatabases:\s*\[[\s\S]*"ClickHouse",\s*"Doris",\s*"DuckDB"/);
-assert.match(connect, /connectionOption\.dbType == 'Doris' \|\|[\s\S]*connectionOption\.dbType == 'ClickHouse'/);
-assert.match(connect, /\[[^\]]*'MySQL'[^\]]*'Doris'[^\]]*'ClickHouse'[^\]]*\]\.includes\(connectionOption\.dbType\)/);
+assert.match(connect, /\[[^\]]*'Doris'[^\]]*'ClickHouse'[^\]]*'S3'[^\]]*\]\.includes\(connectionOption\.dbType\)/);
+assert.match(connect, /\[[^\]]*'MySQL'[^\]]*'Doris'[^\]]*'ClickHouse'[^\]]*'ElasticSearch'[^\]]*\]\.includes\(connectionOption\.dbType\)/);
 assert.match(connect, /case "Doris":[\s\S]*this\.connectionOption\.user = "root";[\s\S]*this\.connectionOption\.password = "";[\s\S]*this\.connectionOption\.port = 9030;[\s\S]*this\.connectionOption\.database = "";[\s\S]*this\.connectionOption\.useSSL = false;/);
 
 const logo = read("resources/icon/doris.svg");
--- test/kingbaseUiConfig.test.js
+++ test/kingbaseUiConfig.test.js
@@ -6,8 +6,8 @@ const source = fs.readFileSync(path.resolve(__dirname, "../src/vue/connect/index
 
 assert.match(source, /KingbaseES:\s*\{[\s\S]*icon: require\("@\/\.\.\/resources\/icon\/kingbase\.svg"\)[\s\S]*text:\s*"KB"/);
 assert.match(source, /supportDatabases:\s*\[[\s\S]*"PostgreSQL"[\s\S]*"KingbaseES"/);
-assert.match(source, /connectionOption\.dbType == 'KingbaseES'/);
-assert.match(source, /\[[^\]]*'KingbaseES'[^\]]*\]\.includes\(connectionOption\.dbType\)/);
+assert.match(source, /\[[^\]]*'KingbaseES'[^\]]*'S3'[^\]]*\]\.includes\(connectionOption\.dbType\)/);
+assert.match(source, /\[[^\]]*'KingbaseES'[^\]]*'ElasticSearch'[^\]]*\]\.includes\(connectionOption\.dbType\)/);
 assert.match(
   source,
   /case "KingbaseES":[\s\S]*this\.connectionOption\.user = "system";[\s\S]*this\.connectionOption\.encrypt = false;[\s\S]*this\.connectionOption\.port = 54321;[\s\S]*this\.connectionOption\.database = "test";/
